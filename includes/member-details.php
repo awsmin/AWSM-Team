68a5403fdef6bd5c72aa0a3a172ebc98c1c05a16
Anantajit JG <anantajitjg2010@gmail.com>
2020-05-06 22:53:13 +0530

    UPDATE: Member details meta-box HTML structure

--- includes/member-details.php
+++ includes/member-details.php
@@ -4,12 +4,12 @@
     <input class="widefat" type="text" name="awsm-team-designation" id="awsm-team-designation" value="<?php echo esc_attr(get_post_meta($post->ID, 'awsm-team-designation', true));?>"/>
 </p>
 <p>
-	<label for="awsm-team-short-desc"><?php _e( 'Short Description (In 140 characters or less)', 'awsm-team' ); ?></label><br/>
+	<label for="awsm-team-short-desc"><?php _e( 'Short Description (In 140 characters or less)', 'awsm-team' ); ?></label>
 	<textarea id="awsm-team-short-desc" name="awsm-team-short-desc" class="widefat" type="text" maxlength="140"><?php echo esc_attr(get_post_meta($post->ID, 'awsm-team-short-desc', true));?></textarea>
 </p>
 </div>
 
-<h3><?php _e('Links (Twitter, LinkedIn, etc)','awsm-team');?></h3>
+<p class="awsm-sorable-table-label"><?php esc_html_e( 'Links (Twitter, LinkedIn, etc)', 'awsm-team' ); ?></p>
 <div class="member-details-section">
 <table id="repeatable-fieldset-two" width="100%" class="awsm-sorable-table">
 	<thead>
@@ -21,7 +21,7 @@
 		</tr>
 	</thead>
 	<tbody>
-		<?php if ( $awsm_social ) : 
+		<?php if ( $awsm_social ) :
 		foreach ( $awsm_social as $field ) { ?>
 		<tr>
 			<td><span class="dashicons dashicons-move"></span></td>
@@ -30,8 +30,8 @@
 			</td>
 			<td><input type="text" placeholder="<?php _e('ex: http://www.twitter.com/awsmin','awsm-team');?>" class="widefat" name="awsm-team-link[]" value="<?php if(isset($field['link'])) echo esc_attr( $field['link'] ); ?>"/></td>
 			<td><a class="button remove-row" href="#"><?php _e('Remove','awsm-team');?></a></td>
-		</tr>	
-		<?php } else: ?> 
+		</tr>
+		<?php } else: ?>
 		<tr>
 			<td><span class="dashicons dashicons-move"></span></td>
 			<td>
@@ -39,7 +39,7 @@
 			</td>
 			<td><input type="text" placeholder="<?php _e('ex: http://www.twitter.com/awsmin','awsm-team');?>" class="widefat" name="awsm-team-link[]" value=""/></td>
 			<td><a class="button remove-row" href="#"><?php _e('Remove','awsm-team');?></a></td>
-		</tr>	
+		</tr>
 		<?php endif; ?>
 		<tr class="empty-row screen-reader-text">
 			<td><span class="dashicons dashicons-move"></span></td>
