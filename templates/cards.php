3d1abbc489a06ca3e9e3de4df614064dd1e845a9
Anantajit JG <anantajitjg2010@gmail.com>
2021-11-25 11:13:36 +0530

    IMPROVE: Card styles

--- templates/cards.php
+++ templates/cards.php
@@ -19,24 +19,28 @@ if ( in_array( $options['preset'], array( 'style-2' ) ) ) {
 		while ( $team->have_posts() ) :
 			$team->the_post();
 			$teamdata = $this->get_options( 'awsm_team_member', $team->post->ID );
+
+			$personal_info = sprintf( '<div class="awsm-personal-info"><h3>%1$s</h3><span>%2$s</span></div>', get_the_title(), wp_kses( $teamdata['awsm-team-designation'], 'post' ) );
 			?>
 				<div id="<?php echo esc_attr( $this->add_id( array( 'awsm-member', $id, $team->post->ID ) ) ); ?>" class="awsm-grid-card">
 				   <figure>
 						<?php
 							$this->checkprint( '<div class="awsm-flip-front">', $flip );
 							echo $this->get_team_thumbnail( $team->post->ID ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+						if ( $flip ) {
+							echo $personal_info; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+						}
 							$this->checkprint( '</div>', $flip );
 						?>
 						 <figcaption class="<?php echo esc_attr( $this->addclass( $flipclass ) ); ?>">
-							<?php $this->checkprint( '<div class="awsm-flip-back-inner">', $flip ); ?>
-							<div class="awsm-personal-info">
-							   <h3><?php the_title(); ?></h3>
-							   <span><?php echo wp_kses( $teamdata['awsm-team-designation'], 'post' ); ?></span>
-							</div> <!-- .awsm-personal-info -->
+							<?php
+								$this->checkprint( '<div class="awsm-flip-back-inner">', $flip );
+								echo $personal_info; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+							?>
 							<div class="awsm-contact-info">
 								<?php
-								$this->checkprint( '<p>%s</p>', wp_kses( $teamdata['awsm-team-short-desc'], 'post' ) );
-								include $this->settings['plugin_path'] . 'templates/partials/social.php';
+									$this->checkprint( '<p>%s</p>', wp_kses( $teamdata['awsm-team-short-desc'], 'post' ) );
+									include $this->settings['plugin_path'] . 'templates/partials/social.php';
 								?>
 							</div> <!-- .awsm-contact-info -->
 							<?php $this->checkprint( '</div><!-- .awsm-flip-back-inner -->', $flip ); ?>
