e28cef33b7156cd1df78fc400d4233655fd7e064
nithinjohn22 <nithin@awsm.in>
2023-10-18 17:04:00 +0530

    phpcs fixes

--- templates/partials/social.php
+++ templates/partials/social.php
@@ -10,17 +10,18 @@ if ( ! empty( $teamdata['awsm_social'] ) ) {
 	foreach ( $teamdata['awsm_social'] as $social ) {
 
 		$social_link = '';
-		$link         = isset( $social['link'] ) ? $social['link'] : '';
-		if ( filter_var( $link, FILTER_VALIDATE_EMAIL ) ) {
-			$social_link = sprintf( 'href="mailto:%1$s"', esc_attr( $link ) );
-		} elseif ( $this->validate_phone_number( $link ) === true ) {
-			$social_link = sprintf( 'href="tel:%1$s"', esc_attr( $link ) );
+		$links       = isset( $social['link'] ) ? $social['link'] : '';
+		$icon        = isset( $social['icon'] ) ? $social['icon'] : '';
+		if ( filter_var( $links, FILTER_VALIDATE_EMAIL ) ) {
+			$social_link = sprintf( 'href="mailto:%1$s"', esc_attr( $links ) );
+		} elseif ( $this->validate_phone_number( $links ) === true ) {
+			$social_link = sprintf( 'href="tel:%1$s"', esc_attr( $links ) );
 		} else {
 
-			$social_link = sprintf( 'href="%1$s" target="_blank"', esc_url( $link ) );
+			$social_link = sprintf( 'href="%1$s" target="_blank"', esc_url( $links ) );
 		}
 
-		echo '<span><a ' . $social_link . '><i class="awsm-icon-' . esc_attr( $social['icon'] ) . '" aria-hidden="true"></i></a></span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo '<span><a ' . $social_link . '><i class="awsm-icon-' . esc_attr( $icon ) . '" aria-hidden="true"></i></a></span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 	echo '</div>';
 }
