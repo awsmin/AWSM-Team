21e8c0f5cb26ee985ffe4ee2041d242815bee67f
nithinjohn22 <nithin@awsm.in>
2023-10-18 13:09:02 +0530

    FIX: Undefined index

--- templates/partials/social.php
+++ templates/partials/social.php
@@ -10,14 +10,14 @@ if ( ! empty( $teamdata['awsm_social'] ) ) {
 	foreach ( $teamdata['awsm_social'] as $social ) {
 
 		$social_link = '';
-		if ( isset( $social['link'] ) ) {
-			if ( filter_var( $social['link'], FILTER_VALIDATE_EMAIL ) ) {
-				$social_link = sprintf( 'href="mailto:%1$s"', esc_attr( $social['link'] ) );
-			} elseif ( $this->validate_phone_number( $social['link'] ) === true ) {
-				$social_link = sprintf( 'href="tel:%1$s"', esc_attr( $social['link'] ) );
-			} else {
-				$social_link = sprintf( 'href="%1$s" target="_blank"', esc_url( $social['link'] ) );
-			}
+		$link         = isset( $social['link'] ) ? $social['link'] : '';
+		if ( filter_var( $link, FILTER_VALIDATE_EMAIL ) ) {
+			$social_link = sprintf( 'href="mailto:%1$s"', esc_attr( $link ) );
+		} elseif ( $this->validate_phone_number( $link ) === true ) {
+			$social_link = sprintf( 'href="tel:%1$s"', esc_attr( $link ) );
+		} else {
+
+			$social_link = sprintf( 'href="%1$s" target="_blank"', esc_url( $link ) );
 		}
 
 		echo '<span><a ' . $social_link . '><i class="awsm-icon-' . esc_attr( $social['icon'] ) . '" aria-hidden="true"></i></a></span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
